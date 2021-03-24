37bcad5881f52263cebb89735f93ac5ac3c315a1
Luis Sandoval <richysandoval@gmail.com>
2021-03-23 21:50:38 -0600

    Initial structure of the admin side

--- UserInterface/src/App.js
+++ UserInterface/src/App.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://awen-media.com">
-        Awen Neighborhood
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App
-        </Typography>
-        <Copyright />
-      </Box>
-    </Container>
+    <Dashboard />
   );
 }
 
